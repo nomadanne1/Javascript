8092339a01d14388b0042b8bd88a3143c4555969
CookieYi <nomadanne1@naver.com>
2021-07-03 22:17:03 +0900

    메소드구현, 상속, super

--- js_object_oriented_programming/08_class.js
+++ js_object_oriented_programming/08_class.js
@@ -13,12 +13,39 @@ class Person { // 8. class : 객체를 만드는 공장 >>
         this.name = name;
         this.first = first;
         this.second = second;
-        console.log('constructor'); // 9. constructor() : class의 Constructor Function
+        // console.log('constructor'); // 9. constructor() : class의 Constructor Function
+    }
+    sum(){ // 10. 메소드 구현 >> Person.prototype.sum = function(){ return ....} 와 의미가 같다.
+        return this.first+this.second; 
+    }
+    // avg(){ // 11. 상속 >> Person이 만든 모든 객체에 avg() 함수를 사용하는 것이 부담스러울 때
+    //     return (this.first+this.second)/2; 
+    // }
+}
+
+class PersonPlus extends Person{
+    // super() >> constructor 실행될때 부모가 생성자가 먼저 실행될 수 있도록
+    // super() - 부모클래스의 생성자
+    constructor(name, first, second, third){
+        super(name, first, second);
+        this.third = third;
+    }
+    // super. - 부모클래스
+    sum(){
+        return super.sum()+this.third;
+    }
+    avg(){
+        return (this.first+this.second+this.third)/3;
     }
 }
 
-var kim = new Person();
-console.log('kim', kim)
+var kim= new PersonPlus('kim', 10, 20, 30); // 12. super() >> 여기에 third를 넣고 싶다. 자식이 못하는일을 부모에게 시킨다.
+console.log("kim.sum()", kim.sum());
+console.log("kim.avg()", kim.avg());
+// ~ 상속 구현
+
+// var kim = new Person();
+// console.log('kim', kim)
 
 // var kim = new Person('kim', 10, 20);
 // kim.sum = function(){
