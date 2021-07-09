6d6ce86c7a07e27e3f8f3a158f5d10dc5a3f7f35
CookieYi <nomadanne1@naver.com>
2021-07-09 09:11:40 +0900

    생산자 함수를 통한상속 : 부모와 연결하기

--- js_object_oriented_programming/16_constructor_inheritance.js
+++ js_object_oriented_programming/16_constructor_inheritance.js
@@ -11,12 +11,18 @@ function PersonPlus(name, first, second, third){
     // this.name = name;
     // this.first = first;
     // this.second = second;
-    Person.call(this, name,first,second); // ★ 첫번째 인자 this
+    Person.call(this, name,first,second); // ★ 첫번째 인자 this & .call 부모를 호출
     this.third = third;
 }
+// PersonPlus.prototype.__proto__ = Person.prototype; // PersonPlus랑 Person 연결
+PersonPlus.prototype = Object.create(Person.prototype);
+PersonPlus.prototype.constructor = PersonPlus; // kim.constructor : [Function: Person] >> [Function: PersonPlus] 변경
+
 PersonPlus.prototype.avg = function(){
     return (this.first+this.second+this.third)/3;
 }
+
 var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
+console.log("kim.constructor", kim.constructor)
