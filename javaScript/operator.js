36353f473cc402e3bf77835425ec622a9c8d02b1
CookieYi <nomadanne1@naver.com>
2021-07-15 23:29:30 +0900

    operator

---
+++ javaScript/operator.js
@@ -0,0 +1,65 @@
+// 1. Stirng concatenation
+console.log('my' + ' cat'); // my cat
+console.log('1' + 2); // 12
+console.log(`string literals: 1 + 2 = ${1 + 2}`); // string literals: 1 + 2 = 3
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preInrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`); // preIncrement: 3, counter: 3
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`); // postIncrement: 3, counter: 4
+const preDecrement = --counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`); // preDecrement: 3, counter: 3
+const postDecrement = counter--;
+console.log(`postDecremen: ${postDecrement}, counter: ${counter}`); // postDecremen: 3, counter: 2
+
+// 4. Assigment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y; // x = x - y; 
+x *= y; // x = x * y; 
+x /= y; // x = x / y; 
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = true;
+const value2 = 4 < 2;
+
+// || (or), find the first truthy value
+console.log(`or: ${value1 || value2 || check()}`); //
+
+// && (and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`);
+
+// often used to compress long if-statement
+// nullableObject && nullableObject.something
+if (nullableObject != null){
+    nullableObject.something;
+}
+
+function check(){
+    for (let i = 0; i < 10; i++){
+        //wasting time
+        console.log('으악');
+    }
+    return true;
+}
