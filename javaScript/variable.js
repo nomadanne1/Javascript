f7562bc80c938877ba3dfb8f50d1eee6c8bad61d
CookieYi <nomadanne1@naver.com>
2021-07-15 23:03:08 +0900

    +추가

--- javaScript/variable.js
+++ javaScript/variable.js
@@ -3,7 +3,7 @@
 // use this for Valina Javascript.
 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/write)
 // let (added in ES6)
 let globalName = 'golobal name';
 {
@@ -27,15 +27,21 @@ console.log(globalName);
 }
 console.log(age); // 4
 
-// 3. Contants
+// 3. Contant, r(read only)
+// use const whenever possible
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 // - security
 // - thread safety
 // - reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
-// Variable types
+// 4. Variable types
 // primitive, single item: number, string, boolean, null, undefined, symbol
 // object, box container
 // function, first-class funtion (일급함수 - 변수에 할당가능, 인자로 전달도 가능, 함수의 리턴타입으로도 사용가능)
