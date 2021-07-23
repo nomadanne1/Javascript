322c851203f8356aa5087460ecd943a327f894ba
CookieYi <nomadanne1@naver.com>
2021-07-23 23:29:04 +0900

    Inheritance

--- javaScript/class.js
+++ javaScript/class.js
@@ -55,3 +55,68 @@ class User {
 const user1 = new User('Steve', 'job', -1);
 console.log(user1.age); // 0 >> _age 지만 .age로 호출 가능한 이유 >> getter, setter 이용하기 떄문에
 
+// 3. Fields (public, private)
+// Too soon!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Refe
+class Experiment {
+    publicField = 2;
+    #privateField = 0;  // 클래스 내부에서만 값 접근, 변경.. 가능
+}
+const experiment = new Experiment();
+console.log(experiment.publicField); // 2
+console.log(experiment.privateField); // undefined
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber){
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(article1.publisher) // undefined >> static은 object마다 할당되는게 아니라 Article 클래스 자체에 붙어있다.
+console.log(Article.publisher); // Dream Coding
+Article.printPublisher(); // Dream Coding
+
+// 5. Inheritance 
+// a way for one class to extend another class/
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    draw() {
+        super.draw();
+        console.log('확장')
+    }
+    getArea() { // 오버라이딩 (재정의)
+        return (this.width * this.height) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw(); 
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
