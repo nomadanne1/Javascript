99459fa5f250161711ae60b812f4187a1f5c39c0
CookieYi <nomadanne1@naver.com>
2021-07-25 22:10:20 +0900

    function quiz

--- javaScript/function.js
+++ javaScript/function.js
@@ -150,4 +150,25 @@ const simpleMultiply = (a, b) => {
     console.log('IIFE');
 })();
 
+// Fun quiz time
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+    switch(command){
+        case 'add':
+            return a + b;
+        case 'subsctract':
+            return a - b;
+        case 'divide':
+            return a / b;
+        case 'multiply':
+            return a * b;
+        case 'remainder':
+            return a % b;
+        default:
+            throw Error('unknown command');
+    }
+}
+console.log(calculate('add', 2, 3)); //5
 
