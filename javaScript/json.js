2df8e4978bf6bcd9efe382817dda2d5ab6084c3a
CookieYi <nomadanne1@naver.com>
2021-08-07 23:02:04 +0900

    stringify, parse

--- javaScript/json.js
+++ javaScript/json.js
@@ -22,7 +22,7 @@ const rabbit = {
     color: 'white',
     size: null,
     birthDate: new Date(),
-    symbol: Symbol('id'),
+    // symbol: Symbol('id'),
     jump: () => {
         console.log(`${this.name} can jump!`)
     },
@@ -30,8 +30,50 @@ const rabbit = {
 
 json = JSON.stringify(rabbit);
 console.log(json);
+
 // {"name":"tori","color":"white","size":null,"birthDate":"2021-08-06T14:45:49.100Z"} 
 // => Symbol() javascript에만 있는 데이터* , jump() >> object에 있는 data가 아니기 때문에 제외
 
+json = JSON.stringify(rabbit, ["name"]);
+console.log(json);
+// {"name":"tori"}
+// => 해당하는 prop만 json으로 변환
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value ; // ★
+});
+// key: , value: [object Object]
+// key: name, value: tori
+// key: color, value: white
+// key: size, value: null
+// key: birthDate, value: 2021-08-07T13:43:34.237Z
+// key: jump, value: () => {
+//         console.log(`${this.name} can jump!`)
+//     }
+
+console.log(json);
+// {"name":"ellie","color":"white","size":null,"birthDate":"2021-08-07T13:45:02.860Z"}
+
 // 2. JSON to Object
 // parse(json)
+console.clear();
+// json = JSON.stringify(rabbit);
+// const obj = JSON.parse(json);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+// {
+//     name: 'tori',
+//     color: 'white',
+//     size: null,
+//     birthDate: '2021-08-07T13:49:11.699Z'
+//   }
+rabbit.jump();
+// obj.jump(); // error *rabbit => json 으로 변환할때 함수는 포함되지 않았기 때문.
+
+console.log(rabbit.birthDate.getDate()); // 7
+// console.log(obj.birthDate.getDate()) // error *birthDate가 String이기 때문
+console.log(obj.birthDate.getDate()); // 7
