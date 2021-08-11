41d2a510bab0ae0e17f0c898ec31780bced2691c
CookieYi <nomadanne1@naver.com>
2021-08-11 23:51:21 +0900

    promise - Error Handling

--- javaScript/async/promise.js
+++ javaScript/async/promise.js
@@ -41,3 +41,32 @@ fetchNumber
     });
 })
 .then(num => console.log(num));
+
+// 4. Error Handling
+const getHen = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve('chicken'), 1000);
+    });
+const getEgg = hen => 
+    new Promise((resolve, reject)=> {
+        // setTimeout(()=> resolve(`${hen}=> egg`),1000);
+        setTimeout(()=> reject(new Error(`error! ${hen} => egg`)),1000);
+    });
+const cook = egg =>
+    new Promise((resolve, reject)=> {
+        setTimeout(()=> resolve(`${egg} => fried egg`), 1000)
+    })
+
+// getHen()
+// .then(hen => getEgg(hen))
+// .then(egg => cook(egg))
+// .then(meal => console.log(meal));
+
+getHen() // 생략가능
+    .then(getEgg)
+    .catch(error =>{ // 오류를 잘 처리 하자
+        return 'bread';
+    })
+    .then(cook)
+    .then(console.log)
+    .catch(console.log);
