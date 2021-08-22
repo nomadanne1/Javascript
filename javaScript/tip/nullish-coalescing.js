bc28a3d41d56461006bc3b4b6d0ac7aca7e0a9ac
CookieYi <nomadanne1@naver.com>
2021-08-22 15:27:57 +0900

    nullish-coalesciong  +

--- javaScript/tip/nullish-coalescing.js
+++ javaScript/tip/nullish-coalescing.js
@@ -2,23 +2,20 @@
 // leftExpr ?? rightExpr
 // 왼쪽코드가 null, undefined인 경우에만 오른쪽코드가 실행된다.
 
-// leftExpr || rightExpr
-// 왼쪽코드가 falsy인경우에 오른쪽 실행된다.
-
 // Bad Code
-function printMessage(text){
-    let message = text;
-    if (text == null || text == undefined){
-        message = 'Nothing to display';
-    }
-    console.log(message);
-}
+// function printMessage(text){
+//     let message = text;
+//     if (text == null || text == undefined){
+//         message = 'Nothing to display';
+//     }
+//     console.log(message);
+// }
 
 // Good Code
-function printMessage(text){
-    const message = text ?? 'Nothing to display';
-    console.log(message);
-}
+// function printMessage(text){
+    // const message = text ?? 'Nothing to display';
+    // console.log(message);
+// }
 
 // cf. Default parameter is only for undefined >> null은 null로 출력된다.
 function printMessage(text='Nothing to display'){
@@ -26,5 +23,31 @@ function printMessage(text='Nothing to display'){
 }
 
 printMessage('Hello');
-printMessage(undefined);
-printMessage(null);
+printMessage(undefined); // Nothing to display
+printMessage(null); // null
+
+// leftExpr || rightExpr
+// 왼쪽코드가 falsy인경우에 오른쪽 실행된다.
+
+// Logical OR operator ||
+function printMessage(text) {
+    const message = text || 'Nothing to display';
+    console.log(message);
+}
+
+printMessage('Hello'); // Hello
+printMessage(undefined); // Nothing to display
+printMessage(null); // Nothing to display
+printMessage(0); // Nothing to display
+printMessage(''); // Nothing to display
+
+const result = getInitialState() ?? fetchFromServer(); // 실행된 값을 할달할때 쓰기도한다.
+console.log(result);
+
+function getInitialState(){
+    return null;
+}
+
+function fetchFromServer(){
+    return 'Hiya from'
+}
