11e86142c97219a9b716c8aa56f0ef75a7e61b32
CookieYi <nomadanne1@naver.com>
2021-09-04 18:42:30 +0900

    배열생성과 사용하기

---
+++ javaScript/newlecture/array.js
@@ -0,0 +1,20 @@
+var nums = new Array();
+
+// nums.push(3);
+// nums.push(21);
+// nums.push(56);
+
+// console.log(nums);
+
+// var n1 = nums.pop();
+
+// console.log("n1: " + n1 +", "+ nums);
+
+nums[3] = 57;
+
+console.log(nums); // [ <3 empty items>, 57 ]
+
+nums.pop();
+console.log(nums);
+nums.pop();
+console.log(nums);
