78ceb48e4e449fbcfb7061e49f4af48c861bfcbe
CookieYi <nomadanne1@naver.com>
2021-09-09 19:47:12 +0900

    JSON 파서를 이용한 파싱

--- javaScript/newlecture/json.js
+++ javaScript/newlecture/json.js
@@ -1,4 +1,4 @@
-// eval 함수와 JSON 파싱하기
+// 10. eval 함수와 JSON 파싱하기
 
 // eval
 
@@ -13,3 +13,20 @@ var data = '[ \
 
 eval("var ar = " + data + ";");
 console.log(ar[0].co); // 0.6
+
+// 11. JSON 파서를 이용한 파싱
+
+// var data2 = JSON.parse('{ id:1, title:"aaa" }'); 
+// key 문자열로 표현안해도 괜찮! (생략가능) >> 일반적
+
+// but!! JSON.parse는 까탈스러워서 key 꼭 문자열로 표현해줘야한다
+var data2 = JSON.parse('{ "id":1, "title":"aaa" }'); 
+console.log(data2) // { id: 1, title: 'aaa' }
+
+var data3 = {id:2, title:"bbb"};
+var json = JSON.stringify(data3); // {"id":2,"title":"bbb"}
+console.log(json);
+
+// cf. var data { "n id": 1, title: "aaa" } // n id 와 같이 문자열 key에 띄어쓰기 포함가능
+// console.log(data["n id"]) // 사용할때 data.n id 요렇게 불가능 하니깐 data["n id"]사용
+
