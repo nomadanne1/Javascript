06fa4e67eeb8ccc96c86e7488d8dd3db4ce8ffa2
CookieYi <nomadanne1@naver.com>
2021-09-11 23:35:24 +0900

    자바스크립트 제어구조

---
+++ javaScript/newlecture/for-in.js
@@ -0,0 +1,14 @@
+var arr = ["hello", "hi", "greeting"];
+var obj = {id:1, titlte:"hello", writeId:"newlec"}
+
+// for(var i =0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for (var i in arr){
+//     console.log(arr[i])
+// } // 배열
+
+for( var i in obj){
+    console.log(obj[i])
+} // 객체
