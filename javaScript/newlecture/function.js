f4f0d66b4096353d156e4da7bf365d555e01b70c
CookieYi <nomadanne1@naver.com>
2021-09-13 22:30:52 +0900

    함수의 가변 인자 argumetns 콜렉션

--- javaScript/newlecture/function.js
+++ javaScript/newlecture/function.js
@@ -5,6 +5,10 @@
 // };
 
 function add(x,y){
+    console.log(typeof arguments[0]); // 매개변수 자료형이 제 각각 > typeof 타입검사
     return x+y;
 }
-console.log(add(3,4));
+// console.log(add(3,4));
+
+// 함수의 가변인자 arguments 콜렉션
+console.log(add(6, 4, 5, 6, 7, "hello")); // 10
