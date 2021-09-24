980805d5f793048d9081117c9255cc684bd01cbc
CookieYi <nomadanne1@naver.com>
2021-09-24 23:37:43 +0900

    Selectors API

--- javaScript/newlecture/javascript1-dom.js
+++ javaScript/newlecture/javascript1-dom.js
@@ -1,3 +1,22 @@
+// Ex3 : Selectors API Level1
+window.addEventListener("load", function(){
+    var section2 = document.getElementById("section2");
+
+    var txtX = section2.getElementsByClassName("txt-x")[0];
+    var txtY = section2.getElementsByClassName("txt-y")[0];
+    var btnAdd = section2.getElementsByClassName("btn-add")[0];
+    var txtSum = section2.getElementsByClassName("txt-sum")[0];
+
+    btnAdd.onclick = function(){
+        var x = parseInt(txtX.value);
+        var y = parseInt(txtY.value);
+
+        txtSum.value = x+y;
+    };
+});
+
+
+
 // Ex2 : 엘리먼트 선택방법 개선하기
 window.addEventListener("load", function(){
     var section2 = document.getElementById("section2");
