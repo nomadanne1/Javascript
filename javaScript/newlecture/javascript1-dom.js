9f97f1b2fbc8b8c01f9d9fe9fa7dbd742d351998
CookieYi <nomadanne1@naver.com>
2021-09-25 23:16:52 +0900

    Node와 Element Node 그리고 childNodes, children

--- javaScript/newlecture/javascript1-dom.js
+++ javaScript/newlecture/javascript1-dom.js
@@ -1,11 +1,29 @@
+// Ex4 : childeNodes를 이용한 노드 선택
+window.addEventListener("load", function(){
+    var section4 = document.querySelector("#section4");
+    var box = section4.querySelector(".box");
+
+    // childNodes :  *comment, text도 자식이다!!
+    // var input1 = box.childNodes[0]; 
+    // var input2 = box.childNodes[1];
+
+    // children : 태그 형태만 자식으로 취급한다.
+    var input1 = box.children[0]; 
+    var input2 = box.children[1];
+
+    input1.value = "hello";
+    input2.value = "okay";
+
+});
+
 // Ex3 : Selectors API Level1
 window.addEventListener("load", function(){
-    var section2 = document.getElementById("section2");
+    var section3 = document.getElementById("section3");
 
-    var txtX = section2.getElementsByClassName("txt-x")[0];
-    var txtY = section2.getElementsByClassName("txt-y")[0];
-    var btnAdd = section2.getElementsByClassName("btn-add")[0];
-    var txtSum = section2.getElementsByClassName("txt-sum")[0];
+    var txtX = section3.querySelector("input[name='x']"); 
+    var txtY = section3.querySelector(".txt-y");
+    var btnAdd = section3.querySelector(".btn-add");
+    var txtSum = section3.querySelector(".txt-sum");
 
     btnAdd.onclick = function(){
         var x = parseInt(txtX.value);
@@ -14,8 +32,6 @@ window.addEventListener("load", function(){
         txtSum.value = x+y;
     };
 });
-
-
 
 // Ex2 : 엘리먼트 선택방법 개선하기
 window.addEventListener("load", function(){
