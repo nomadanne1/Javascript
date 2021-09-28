9c8cbc7fa75c7252dbf02d3c0f51e2d15d410b95
CookieYi <nomadanne1@naver.com>
2021-09-28 22:12:25 +0900

    엘리먼트 노드의 속성 다루기

--- javaScript/newlecture/javascript1-dom.js
+++ javaScript/newlecture/javascript1-dom.js
@@ -1,3 +1,17 @@
+// Ex5 : 엘리먼트 노드의 속성 변경
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section5");
+    var srcInput = section.querySelector(".src-input");
+    var imgSelect = section.querySelector(".img-select")
+    var changeButton = section.querySelector(".change-button");
+    var img = section.querySelector(".img");
+    
+    changeButton.onclick=function(){
+        img.src = "images/"+srcInput.value;
+        // img.src = "images/"+imgSelect.value;
+    }
+});
+
 // Ex4 : childeNodes를 이용한 노드 선택
 window.addEventListener("load", function(){
     var section4 = document.querySelector("#section4");
