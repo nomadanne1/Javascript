d7517787da1153604c5a5e69cfcf72608c3a098e
CookieYi <nomadanne1@naver.com>
2021-10-02 23:54:44 +0900

    노드 복제 및 템플릿 복제

--- javaScript/newlecture/javascript1-dom.js
+++ javaScript/newlecture/javascript1-dom.js
@@ -1,3 +1,50 @@
+// Ex7 :
+window.addEventListener("load", function(){
+    var notices = [
+        {id:5, title:"힣", regDate:"2021-01-01", writerId:"이국희", hit:1},
+        {id:6, title:"헤헷", regDate:"2021-01-01", writerId:"이웬디", hit:0}
+    ];
+
+    var section = document.querySelector("#section7");
+
+    var noticeList = section.querySelector(".notice-list");
+    var tbodyNode = noticeList.querySelector("tbody");
+    var cloneButton = section.querySelector(".clone-button");
+    var templateButton = section.querySelector(".template-button");
+    
+    cloneButton.onclick = function(){
+        var trNode = noticeList.querySelector("tbody tr");
+        var cloneNode = trNode.cloneNode(true); // true: deep 복제
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTML = '<a href="'+notices[0].id+'">'+notices[0].title+'</a>'
+        
+        // 방법2
+        var aNode = tds[1].children[0]; // a태그
+        aNode.href=notices[0].id;
+        aNode.textContent = notices[0].title;
+
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writerId;
+        tds[4].textContent = notices[0].hit;
+
+        tbodyNode.appendChild(cloneNode);
+    };
+
+    templateButton.onclick = function(){
+        var template = section.querySelector("template");
+        var cloneNode = document.importNode(template.content, true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTML = '<a href="'+notices[0].id+'">'+notices[0].title+'</a>'
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writerId;
+        tds[4].textContent = notices[0].hit;
+
+        tbodyNode.appendChild(cloneNode);
+    };
+});
+
 // Ex6 : 노드조작- 메뉴추가(createTextNode, Element)
 window.addEventListener("load", function(){
     var section = document.querySelector("#section6");
